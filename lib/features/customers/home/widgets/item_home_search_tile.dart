ee757aee596cf0876ad4300f17ba9966397dc159
super-novey <nganhduy2101003@gmail.com>
2024-12-01 00:16:10 +0700

    fix loading menu

--- lib/features/customers/home/widgets/item_home_search_tile.dart
+++ lib/features/customers/home/widgets/item_home_search_tile.dart
@@ -16,69 +16,70 @@ class ItemHomeSearchTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        Get.to(MenuRestaurantScreen(), arguments: item.partnerId);
+        Get.to(MenuRestaurantScreen(
+          userId: item.partnerId,
+        ));
       },
-      
       child: Padding(
-        padding: const EdgeInsets.only(left: MySizes.sm, right: MySizes.sm, top: MySizes.sm),
+        padding: const EdgeInsets.only(
+            left: MySizes.sm, right: MySizes.sm, top: MySizes.sm),
         child: SizedBox(
-            height: 90,
-            child: Card(
-              elevation: 4,
-              shadowColor: MyColors.darkPrimaryColor,
-              child: Padding(
-                padding: const EdgeInsets.only(
-                    top: MySizes.sm,
-                    left: MySizes.md,
-                    bottom: MySizes.sm,
-                    right: MySizes.sm),
-                child: Row(
-                  children: [
-                    ClipRRect(
-                        borderRadius:
-                            BorderRadius.circular(MySizes.borderRadiusMd),
-                        child: CachedNetworkImage(
-                          imageUrl: item.itemImage,
-                          width: 55,
-                          height: 55,
-                          fit: BoxFit.cover,
-                          placeholder: (context, url) => const Center(
-                              child:
-                                  CircularProgressIndicator()), 
-                          errorWidget: (context, url, error) => const Icon(Icons
-                              .error), 
-                        )),
-                    const SizedBox(
-                      width: MySizes.spaceBtwItems,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          item.itemName,
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyLarge!
-                              .apply(color: MyColors.darkPrimaryTextColor),
-                        ),
-                        const SizedBox(
-                          height: MySizes.sm,
-                        ),
-                        Text(
-                          MyFormatter.formatCurrency(item.price),
-                          style: Theme.of(context)
-                              .textTheme
-                              .labelMedium!
-                              .apply(color: MyColors.primaryTextColor),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+          height: 90,
+          child: Card(
+            elevation: 4,
+            shadowColor: MyColors.darkPrimaryColor,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: MySizes.sm,
+                  left: MySizes.md,
+                  bottom: MySizes.sm,
+                  right: MySizes.sm),
+              child: Row(
+                children: [
+                  ClipRRect(
+                      borderRadius:
+                          BorderRadius.circular(MySizes.borderRadiusMd),
+                      child: CachedNetworkImage(
+                        imageUrl: item.itemImage,
+                        width: 55,
+                        height: 55,
+                        fit: BoxFit.cover,
+                        placeholder: (context, url) =>
+                            const Center(child: CircularProgressIndicator()),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error),
+                      )),
+                  const SizedBox(
+                    width: MySizes.spaceBtwItems,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        item.itemName,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge!
+                            .apply(color: MyColors.darkPrimaryTextColor),
+                      ),
+                      const SizedBox(
+                        height: MySizes.sm,
+                      ),
+                      Text(
+                        MyFormatter.formatCurrency(item.price),
+                        style: Theme.of(context)
+                            .textTheme
+                            .labelMedium!
+                            .apply(color: MyColors.primaryTextColor),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
+        ),
       ),
     );
   }
