997ca610a93e19d9598c9e895489a8e25b7c69e7
nghaan1810 <ngochan.tv253@gmail.com>
2024-12-01 18:19:40 +0700

    detail item in customer

--- lib/features/customers/restaurant_list/views/menu_restaurant_list/widgets/menu_restaurant_tile.dart
+++ lib/features/customers/restaurant_list/views/menu_restaurant_list/widgets/menu_restaurant_tile.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery_h2d/features/customers/restaurant_list/controllers/cart_controller.dart';
+import 'package:food_delivery_h2d/features/customers/restaurant_list/views/menu_restaurant_detail/menu_restaurant_detail.dart';
 import 'package:food_delivery_h2d/features/restaurants/menu_management/models/item_model.dart';
 import 'package:food_delivery_h2d/utils/constants/colors.dart';
 import 'package:food_delivery_h2d/utils/constants/sizes.dart';
@@ -15,121 +16,153 @@ class MenuRestaurantTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final cartController = Get.put(CartController());
-    return Padding(
-      padding: const EdgeInsets.only(
-          left: MySizes.sm, right: MySizes.sm, top: MySizes.sm),
-      child: SizedBox(
-        height: 120,
-        child: Card(
-          elevation: 4,
-          shadowColor: MyColors.darkPrimaryColor,
-          child: Padding(
-            padding: const EdgeInsets.only(
-                top: MySizes.sm,
-                left: MySizes.md,
-                bottom: MySizes.sm,
-                right: MySizes.sm),
-            child: Row(
-              children: [
-                ClipRRect(
-                    borderRadius: BorderRadius.circular(MySizes.borderRadiusMd),
-                    child: CachedNetworkImage(
-                      imageUrl: item.itemImage,
-                      width: 55,
-                      height: 55,
-                      fit: BoxFit.cover,
-                      placeholder: (context, url) =>
-                          const Center(child: CircularProgressIndicator()),
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.error),
-                    )),
-                const SizedBox(
-                  width: MySizes.spaceBtwItems,
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        item.itemName,
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyLarge!
-                            .apply(color: MyColors.primaryTextColor),
-                      ),
-                      const SizedBox(
-                        height: MySizes.xs,
-                      ),
-                      Text(
-                        item.description,
-                        style: Theme.of(context)
-                            .textTheme
-                            .labelMedium!
-                            .apply(color: MyColors.secondaryTextColor),
-                      ),
-                      const SizedBox(
-                        height: MySizes.xs,
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            "Còn lại: ${item.quantity.toString()}",
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelSmall!
-                                .apply(color: MyColors.primaryTextColor),
-                          ),
-                          const SizedBox(
-                            width: MySizes.sm,
-                          ),
-                          Container(
-                              color: MyColors.dividerColor,
-                              width: 0.8,
-                              height: 15),
-                          const SizedBox(
-                            width: MySizes.sm,
-                          ),
-                          Text(
-                            "Đã bán: ${item.quantity.toString()}",
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelSmall!
-                                .apply(color: MyColors.primaryTextColor),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: MySizes.xs,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: MySizes.sm),
-                        child: Row(
+    return InkWell(
+      onTap: () {
+        Get.to(MenuRestaurantDetail(item: item));
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(
+            left: MySizes.sm, right: MySizes.sm, top: MySizes.sm),
+        child: SizedBox(
+          height: 135,
+          child: Card(
+            elevation: 4,
+            shadowColor: MyColors.darkPrimaryColor,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: MySizes.sm,
+                  left: MySizes.md,
+                  bottom: MySizes.sm,
+                  right: MySizes.sm),
+              child: Row(
+                children: [
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(MySizes.borderRadiusMd),
+                      child: CachedNetworkImage(
+                        imageUrl: item.itemImage,
+                        width: 55,
+                        height: 55,
+                        fit: BoxFit.cover,
+                        placeholder: (context, url) =>
+                            const Center(child: CircularProgressIndicator()),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error),
+                      )),
+                  const SizedBox(
+                    width: MySizes.spaceBtwItems,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          item.itemName,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyLarge!
+                              .apply(color: MyColors.primaryTextColor),
+                        ),
+                        const SizedBox(
+                          height: MySizes.xs,
+                        ),
+                        Text(
+                          item.description,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelMedium!
+                              .apply(color: MyColors.secondaryTextColor),
+                        ),
+                        const SizedBox(
+                          height: MySizes.xs,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Còn lại: ${item.quantity.toString()}",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelSmall!
+                                  .apply(color: MyColors.primaryTextColor),
+                            ),
+                            const SizedBox(
+                              width: MySizes.sm,
+                            ),
+                            Container(
+                                color: MyColors.dividerColor,
+                                width: 0.8,
+                                height: 15),
+                            const SizedBox(
+                              width: MySizes.sm,
+                            ),
+                            Text(
+                              "Đã bán: ${item.quantity.toString()}",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelSmall!
+                                  .apply(color: MyColors.primaryTextColor),
+                            ),
+                          ],
+                        ),
+                        Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             Text(
-                              '${item.price}đ',
+                              MyFormatter.formatCurrency(item.price),
                               style: Theme.of(context)
                                   .textTheme
                                   .bodyLarge!
                                   .apply(color: MyColors.darkPrimaryTextColor),
                             ),
-                            InkWell(
-                              onTap: () {
-                                cartController.addToCart(item);
-                              },
-                              child: const Icon(
-                                Icons.add_box,
-                                color: MyColors.darkPrimaryTextColor,
-                              ),
-                            ),
+                            Obx(() {
+                            var quantity = cartController
+                                    .itemQuantities[item.itemName] ??
+                                0;
+                            if (quantity > 0) {
+                              return Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  IconButton(
+                                    onPressed: () {
+                                      cartController.removeFromCart(item);
+                                    },
+                                    icon: const Icon(
+                                        Icons.remove_circle_outline_rounded,
+                                        color:
+                                            MyColors.darkPrimaryTextColor),
+                                  ),
+                                  Text(quantity.toString()),
+                                  IconButton(
+                                    onPressed: () {
+                                      cartController.addToCart(item);
+                                    },
+                                    icon: const Icon(Icons.add_circle,
+                                        color:
+                                            MyColors.darkPrimaryTextColor),
+                                  ),
+                                ],
+                              );
+                            } else {
+                              return InkWell(
+                                onTap: () {
+                                  cartController.addToCart(item);
+                                },
+                                child: const Icon(
+                                  Icons.add_box,
+                                  color: MyColors.darkPrimaryTextColor,
+                                ),
+                              );
+                            }
+                          }),
                           ],
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
