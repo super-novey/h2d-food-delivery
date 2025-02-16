c1b91de37c810e1d5b31465f275244f750b10a0a
super-novey <nganhduy2101003@gmail.com>
2025-02-16 22:04:27 +0700

    update slide widget

--- lib/utils/http/http_client.dart
+++ lib/utils/http/http_client.dart
@@ -6,8 +6,8 @@ class HttpHelper {
   static const String _baseUrl =
       //
 
-      //    "http://localhost:8081/api/v1"; // change URL
-      "https://06af-42-113-23-79.ngrok-free.app/api/v1";
+      "http://localhost:8081/api/v1"; // change URL
+  // "https://06af-42-113-23-79.ngrok-free.app/api/v1";
 
   static Future<Map<String, dynamic>> get(String endpoint) async {
     final response = await http.get(Uri.parse('$_baseUrl/$endpoint'));
